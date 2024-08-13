bb5d5dbb0ca64a85c72da546cf9769c3bd90e972
daniishkhan <danishafzalkhan@gmail.com>
2024-08-13 21:30:31 +0500

    added homepage

--- svelte-frontend-service/svelte.config.js
+++ svelte-frontend-service/svelte.config.js
@@ -1,9 +1,17 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import sveltePreprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
+  preprocess: [
+    vitePreprocess(),
+    sveltePreprocess({
+      scss: {
+        prependData: '@import "src/styles/global.scss";'
+      }
+    })
+  ],
 
   kit: {
     adapter: adapter()
