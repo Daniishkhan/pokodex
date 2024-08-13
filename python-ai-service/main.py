3c36a8bf3a1cafe54ec38612bbfdb7345c89a96d
daniishkhan <danishafzalkhan@gmail.com>
2024-08-13 15:11:36 +0500

    updated port to 8001

--- python-ai-service/main.py
+++ python-ai-service/main.py
@@ -1,6 +1,7 @@
 from Scraper import wikiScraper
 from fastapi import FastAPI, exceptions
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
 app = FastAPI()
 
@@ -30,3 +31,6 @@ def getData():
         return {"data": data}
     except Exception as e:
         raise exceptions.HTTPException(status_code=500, detail=str(e))
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8001)
